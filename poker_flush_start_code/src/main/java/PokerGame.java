d2aaab6f683e60b633a617db61b3d4fa19499a60
Piraven Nandakumaran <Xothspath@gmail.com>
2023-10-19 20:41:07 +0100

    Code can detect a flush set of cards

--- poker_flush_start_code/src/main/java/PokerGame.java
+++ poker_flush_start_code/src/main/java/PokerGame.java
@@ -6,6 +6,14 @@ public class PokerGame {
 
     public boolean checkIfFlush(String[] cards){
 //        write code here
+        char initalSuit = cards[0].charAt(cards[0].length() - 1);
+
+        for (String card : cards){
+            if (card.charAt(card.length() - 1) != initalSuit){
+                return false;
+            }
+        }
+
         return true;
     }
 
