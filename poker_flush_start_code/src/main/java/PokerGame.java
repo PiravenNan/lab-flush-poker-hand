49f78a8b756b38793f4e15a0be7d9e2ae00cb5c2
Piraven Nandakumaran <Xothspath@gmail.com>
2023-10-19 20:48:58 +0100

    Altered code to ignore first card in deck in loop as it has already been evaulated

--- poker_flush_start_code/src/main/java/PokerGame.java
+++ poker_flush_start_code/src/main/java/PokerGame.java
@@ -8,8 +8,8 @@ public class PokerGame {
 //        write code here
         char initalSuit = cards[0].charAt(cards[0].length() - 1);
 
-        for (String card : cards){
-            if (card.charAt(card.length() - 1) != initalSuit){
+        for (int i = 1; i<cards.length;i++){
+            if (cards[i].charAt(cards[i].length() - 1) != initalSuit){
                 return false;
             }
         }
